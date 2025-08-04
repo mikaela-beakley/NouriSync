b06b032e5babc4b4808aff2ae5048c19e648cf69
Mikaela <mikaelabeakley@gmail.com>
2025-08-04 18:28:19 -0500

    Write questionnaire.html, create caregiver and patient forms

--- web/src/server.py
+++ web/src/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -13,6 +13,26 @@ def index():
 def test():
     result = 200
     return render_template('questionnaire.html')
+
+@app.route("/questionnaire-caregiver-info", methods=['GET', 'POST'])
+
+def post_caregiver_answers():
+    print(request.form)
+    form_answers = request.form
+    print(form_answers['scale-question-1'])
+    print(form_answers['scale-question-2'])
+    print(form_answers['comments'])
+    return redirect("/questionnaire")
+
+@app.route("/questionnaire-patient-info", methods=['GET', 'POST'])
+
+def post_patient_answers():
+    print(request.form)
+    form_answers = request.form
+    print(form_answers['scale-question-1'])
+    print(form_answers['scale-question-2'])
+    print(form_answers['comments'])
+    return redirect("/questionnaire")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
