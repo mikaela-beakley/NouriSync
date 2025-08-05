270436054ebea25bea022a614ea06d0d1149bafb
Mikaela <mikaelabeakley@gmail.com>
2025-08-04 20:03:01 -0500

    Update routing

--- web/src/server.py
+++ web/src/server.py
@@ -14,15 +14,6 @@ def test():
     result = 200
     return render_template('questionnaire.html')
 
-@app.route("/questionnaire-caregiver-info", methods=['GET', 'POST'])
-
-def post_caregiver_answers():
-    form_answers = request.form
-    print(form_answers['scale-question-1'])
-    print(form_answers['scale-question-2'])
-    print(form_answers['comments'])
-    return redirect("/questionnaire")
-
 @app.route("/questionnaire-patient-info", methods=['GET', 'POST'])
 
 def post_patient_answers():
@@ -32,6 +23,17 @@ def post_patient_answers():
     print(form_answers['scale-question-2'])
     print(form_answers['comments'])
     return redirect("/questionnaire")
+
+@app.route("/questionnaire-caregiver-info", methods=['GET', 'POST'])
+
+def post_caregiver_answers():
+    form_answers = request.form
+    print(form_answers['scale-question-1'])
+    print(form_answers['scale-question-2'])
+    print(form_answers['comments'])
+    return redirect("/")
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
